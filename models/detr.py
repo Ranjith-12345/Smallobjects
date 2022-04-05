28828486babb4a071d09b1787986a5015eb62d4a
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-04-05 15:39:15 +1000

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -55,11 +55,11 @@ class DETR(nn.Module):
         )'''
         #self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         self.input_proj = nn.Sequential(
-             nn.Conv2d(backbone.num_channels, backbone.num_channels, kernel_size=1),
-             nn.BatchNorm2d(backbone.num_channels),
-             nn.ReLU(inplace=False),
-             nn.Conv2d(backbone.num_channels, backbone.num_channels, kernel_size=1),
-             nn.BatchNorm2d(backbone.num_channels),
+          nn.AdaptiveAvgPool2d(1),
+          nn.Conv2d(2048,256, kernel_size=1),
+          nn.BatchNorm2d(256),
+          nn.ReLU(inplace=False),
+          nn.Conv2d(256, 256, kernel_size=1),
         )
         self.backbone = backbone
         self.aux_loss = aux_loss
