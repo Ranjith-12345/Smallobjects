74e8e31ce7e4451812c40341b353a1e203adea87
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-04-06 11:42:32 +1000

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -85,7 +85,7 @@ class DETR(nn.Module):
         src, mask = features[-1].decompose()
         #self.input_proj = self.x(self.input_proj)
         assert mask is not None
-        hs = self.transformer(self.localatt(self.input_proj(src)), mask, self.query_embed.weight, pos[-1])[0]
+        hs = self.transformer(self.input_proj(self.localatt(src)), mask, self.query_embed.weight, pos[-1])[0]
         # hs = 
         outputs_class = self.class_embed(hs)
         outputs_coord = self.bbox_embed(hs).sigmoid()
